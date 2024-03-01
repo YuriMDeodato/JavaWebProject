dcf47bedede87ad59c08ce44a919c57e3aef0e82
YuriMDeodato <146349459+YuriMDeodato@users.noreply.github.com>
2024-03-01 11:53:56 -0300

    add class java

---
+++ Calculos.java
@@ -0,0 +1,16 @@
+package br.edu.senaisp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/calculo");
+
+public class Calculos extends HttpServlet {
+
+	
+}
